3b23bd31aa5038e5acd7e26596d5996b9de21811
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-28 22:12:10 +0200

    feat: function verify_args

---
+++ philo/src/verify.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   verify.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 17:08:02 by bmugnol-          #+#    #+#             */
+/*   Updated: 2022/10/20 21:20:32 by bmugnol-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+static int	is_digit_str(const char *str);
+
+int	verify_args(int argc, char **argv)
+{
+	size_t		index;
+
+	if (argc != 5 && argc != 6)
+		return (generic_error(INCORRECT_USAGE, argv[0],
+				"Invalid argument",
+				"./philo <number_of_philosophers> <time_to_die> <time_to_eat> \
+<time_to_sleep> [<number_of_times_each_philosopher_must_eat>]"));
+	index = 1;
+	while (argv[index])
+	{
+		if (!is_digit_str(argv[index]))
+			return (generic_error(INCORRECT_USAGE, argv[0],
+					"Invalid argument: numeric argument required", NULL));
+		if (ft_atoi(argv[index]) <= 0)
+			return (generic_error(INCORRECT_USAGE, argv[0],
+					"Invalid argument: argument must be positive", NULL));
+		index++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	is_digit_str(const char *str)
+{
+	if (!str || !*str || !ft_isdigit(*str))
+		return (0);
+	str++;
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
