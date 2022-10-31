422df0ef306641a1d90d1d2b80532fe68474c6e4
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-31 22:16:55 +0100

    normify

--- philo/src/setup.c
+++ philo/src/setup.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 21:20:35 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/10/31 21:40:17 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/10/31 22:16:28 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ t_config	*get_config(int argc, char **argv)
 	if (!config)
 		return (NULL);
 	config->philosopher_count = ft_atoi(argv[1]);
-	config->time_to_die_usec = (long)(ft_atoi(argv[2])) * MILLI_TO_MICRO;
-	config->time_to_eat_usec = (long)(ft_atoi(argv[3])) * MILLI_TO_MICRO;
-	config->time_to_sleep_usec = (long)(ft_atoi(argv[4])) * MILLI_TO_MICRO;
+	config->time_to_die_usec = ft_atoi(argv[2]) * (long)(MILLI_TO_MICRO);
+	config->time_to_eat_usec = ft_atoi(argv[3]) * (long)(MILLI_TO_MICRO);
+	config->time_to_sleep_usec = ft_atoi(argv[4]) * (long)(MILLI_TO_MICRO);
 	if (argv[4])
 		config->minimum_eat_count = ft_atoi(argv[4]);
 	else
