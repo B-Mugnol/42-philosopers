d39e219fe3bf04a1088188dcd7a72fd7f30bd622
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-31 20:55:21 +0100

    added missing config initialization

--- philo/src/setup.c
+++ philo/src/setup.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 21:20:35 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/10/20 22:29:20 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/10/31 20:55:00 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ t_config	*get_config(int argc, char **argv)
 	config->philosopher_count = ft_atoi(argv[1]);
 	config->time_to_die = ft_atoi(argv[2]);
 	config->time_to_eat = ft_atoi(argv[3]);
+	config->time_to_sleep = ft_atoi(argv[4]);
 	if (argv[4])
-		config->minimum_eat_count = ft_atoi(argv[4]);
+		config->minimum_eat_count = ft_atoi(argv[5]);
 	else
 		config->minimum_eat_count = -1;
 	return (config);
