a1207c6440f0f55b38c66db4bf788533a1b138e5
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-31 20:28:24 +0100

    now includes used headers

--- philo/inc/master.h
+++ philo/inc/master.h
@@ -6,11 +6,13 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 21:51:43 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/10/10 21:52:11 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/10/31 20:17:13 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MASTER_H
 # define MASTER_H
+
+# include "philosophers.h"
 
 #endif
