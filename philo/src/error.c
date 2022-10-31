195a2de4efb6e2e7852e5e7be5ec539ee476f000
B-Mugnol <bruno_mugnol@hotmail.com>
2022-10-31 20:27:08 +0100

    removed ft_putendl_fd fn usage

--- philo/src/error.c
+++ philo/src/error.c
@@ -6,11 +6,11 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 21:22:05 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/10/20 21:18:47 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/10/31 20:08:49 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "philosophers.h"
 
 int	generic_error(int exit_code, char *locale, char *msg, char *usage)
 {
@@ -23,11 +23,15 @@ int	generic_error(int exit_code, char *locale, char *msg, char *usage)
 			ft_putstr_fd("\n", 2);
 	}
 	if (msg)
-		ft_putendl_fd(msg, 2);
+	{
+		ft_putstr_fd(msg, 2);
+		ft_putstr_fd("\n", 2);
+	}
 	if (usage)
 	{
 		ft_putstr_fd("Usage: ", 2);
-		ft_putendl_fd(usage, 2);
+		ft_putstr_fd(usage, 2);
+		ft_putstr_fd("\n", 2);
 	}
 	return (exit_code);
 }
