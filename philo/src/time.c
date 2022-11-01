214139878593e5caccd29156beafbff515053234
B-Mugnol <bruno_mugnol@hotmail.com>
2022-11-01 01:10:11 +0100

    added time_from_start; changed return type from long to suseconds_t

--- philo/src/time.c
+++ philo/src/time.c
@@ -6,22 +6,37 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 21:05:03 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/10/31 22:01:17 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/10/31 22:55:19 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/time.h>
 
-long	time_from_point_usec(struct timeval point)
+suseconds_t	time_from_point_usec(struct timeval point)
 {
 	struct timeval	curr_time;
-	long			seconds;
+	suseconds_t		seconds;
 
 	if (gettimeofday(&curr_time, NULL) == -1)
 	{
 		generic_error(EXIT_FAILURE, "gettimeofday()", "function failed", NULL);
 		return (-1);
 	}
-	seconds = (long)(curr_time.tv_sec - point.tv_sec);
+	seconds = (suseconds_t)(curr_time.tv_sec - point.tv_sec);
 	return (seconds * UNIT_TO_MICRO + curr_time.tv_usec - point.tv_usec);
+}
+
+suseconds_t	time_from_start_usec(void)
+{
+	static struct timeval	start_time = {.tv_sec = 0, .tv_usec = 0};
+	struct timeval			curr_time;
+
+	if (gettimeofday(&curr_time, NULL) == -1)
+	{
+		generic_error(EXIT_FAILURE, "gettimeofday()", "function failed", NULL);
+		return (-1);
+	}
+	if (start_time.tv_sec == 0 && start_time.tv_usec == 0)
+		start_time = curr_time;
+	return (time_from_point_usec(start_time));
 }
