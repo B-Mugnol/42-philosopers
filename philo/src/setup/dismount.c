83425dd170fd2fa7edf696738d2f45e584355f2b
B-Mugnol <bruno_mugnol@hotmail.com>
2022-11-15 20:56:16 +0100

    added if statements to protect invalid frees/dismounts/destroys

--- philo/src/setup/dismount.c
+++ philo/src/setup/dismount.c
@@ -6,13 +6,13 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 23:45:45 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/11/02 00:12:30 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/11/13 01:28:56 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	*dismount_forks(int fork_count, pthread_mutex_t *fork)
+void	*dismount_forks(int fork_count, t_fork *fork)
 {
 	int	errno;
 
@@ -20,7 +20,7 @@ void	*dismount_forks(int fork_count, pthread_mutex_t *fork)
 		return (NULL);
 	while (fork_count - 1 >= 0)
 	{
-		errno = pthread_mutex_destroy(&fork[fork_count - 1]);
+		errno = pthread_mutex_destroy(&fork[fork_count - 1].lock);
 		if (errno != 0)
 			generic_error(errno, "dismount_forks()",
 				"mutex_destroy failed", NULL);
@@ -34,11 +34,17 @@ void	*dismount_table(t_table *table)
 {
 	int	errno;
 
-	free(table->philo);
-	dismount_forks(table->philo_count, table->fork);
-	errno = pthread_mutex_destroy(&(table->lock));
-	if (errno != 0)
-		generic_error(errno, "dismount_table()", "mutex_destroy failed", NULL);
+	if (table->philo)
+		free(table->philo);
+	if (table->fork)
+		dismount_forks(table->philo_count, table->fork);
+	if (table->philo && table->fork)
+	{
+		errno = pthread_mutex_destroy(&table->lock);
+		if (errno != 0)
+			generic_error(errno, "dismount_table()", "mutex_destroy failed",
+				NULL);
+	}
 	free(table);
 	return (NULL);
 }
