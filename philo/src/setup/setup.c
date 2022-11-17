1fc816975ffd574adf4befb5fe1316cbeac39459
B-Mugnol <bruno_mugnol@hotmail.com>
2022-11-17 20:50:31 +0100

    config setup fixed, table setup protected

--- philo/src/setup/setup.c
+++ philo/src/setup/setup.c
@@ -6,63 +6,52 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 21:20:35 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/11/02 01:28:01 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/11/13 01:27:54 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "setup.h"
 #include "philosophers.h"
 
-t_config	*get_config(int argc, char **argv)
+t_config	get_config(char **argv)
 {
-	t_config	*config;
+	t_config	config;
 
-	if (verify_args(argc, argv) == INCORRECT_USAGE)
-		return (NULL);
-	config = malloc(sizeof(t_config));
-	if (!config)
-	{
-		generic_error(EXIT_FAILURE, "get_config", "malloc failed", NULL);
-		return (NULL);
-	}
-	config->philosopher_count = ft_atoi(argv[1]);
-	config->time_to_die_usec = ft_atoi(argv[2]) * (suseconds_t)(MILLI_TO_MICRO);
-	config->time_to_eat_usec = ft_atoi(argv[3]) * (suseconds_t)(MILLI_TO_MICRO);
-	config->time_to_sleep_usec = (
+	config.philosopher_count = ft_atoi(argv[1]);
+	config.time_to_die_usec = ft_atoi(argv[2]) * (suseconds_t)(MILLI_TO_MICRO);
+	config.time_to_eat_usec = ft_atoi(argv[3]) * (suseconds_t)(MILLI_TO_MICRO);
+	config.time_to_sleep_usec = (
 			ft_atoi(argv[4]) * (suseconds_t)(MILLI_TO_MICRO)
 			);
-	if (argv[4])
-		config->minimum_eat_count = ft_atoi(argv[4]);
+	if (argv[5])
+		config.minimum_eat_count = ft_atoi(argv[5]);
 	else
-		config->minimum_eat_count = -1;
+		config.minimum_eat_count = -1;
 	return (config);
 }
 
-t_table	*get_table(int philo_count)
+t_table	*get_table(t_config config)
 {
 	t_table	*table;
 	int		errno;
 
 	table = malloc(sizeof(t_table));
 	if (!table)
-	{
-		generic_error(EXIT_FAILURE, "get_table", "malloc failed", NULL);
 		return (NULL);
-	}
-	table->philo_count = philo_count;
-	table->philo = init_philosophers(philo_count, table);
+	table->philo_count = config.philosopher_count;
+	table->has_corpse = 0;
+	table->philo = init_philosophers(config, table);
+	table->fork = NULL;
 	if (!table->philo)
-		return (NULL);
-	table->fork = init_forks(philo_count);
-	if (!table->fork)
-	{
-		free(table->philo);
-		return (NULL);
-	}
-	errno = pthread_mutex_init(&(table->lock), NULL);
-	if (errno != 0)
-		generic_error(errno, "get_table", "mutex_init failed", NULL);
-	if (errno != 0)
 		return (dismount_table(table));
+	table->fork = init_forks(config.philosopher_count);
+	if (!table->fork)
+		return (dismount_table(table));
+	errno = pthread_mutex_init(&table->lock, NULL);
+	if (errno != 0)
+	{
+		generic_error(errno, "get_table()", "mutex_init failed", NULL);
+		return (dismount_table(table));
+	}
 	return (table);
 }
--- philo/src/setup/setup.h
+++ philo/src/setup/setup.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 00:02:59 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/11/02 01:30:57 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/11/13 01:19:01 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 # include "defines.h"
 
 // setup.c
-t_config		*get_config(int argc, char **argv);
-t_table			*get_table(int philo_count);
+t_config	get_config(char **argv);
+t_table		*get_table(t_config config);
 
 // init.c
-t_philo			*init_philosophers(int philo_count, t_table *table);
-pthread_mutex_t	*init_forks(int fork_count);
+t_philo		*init_philosophers(t_config config, t_table *table);
+t_fork		*init_forks(int fork_count);
 
 // dismount.c
-void			*dismount_forks(int fork_count, pthread_mutex_t *fork);
-void			*dismount_table(t_table *table);
+void		*dismount_forks(int fork_count, t_fork *fork);
+void		*dismount_table(t_table *table);
 
 #endif
