297c459a60c9ee5ef383b41dbb4d4e3dc315233c
B-Mugnol <bruno_mugnol@hotmail.com>
2022-11-24 20:31:14 +0100

    added mutex protection to random_bool() calling

--- philo/src/simulation/actions/action_utils.c
+++ philo/src/simulation/actions/action_utils.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 17:10:23 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/11/19 00:49:29 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/11/24 20:26:23 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,15 @@ void	philo_wait_for_fork(t_philo *philo)
 		if (time_from_point_usec(philo->last_ate)
 			>= philo->config.time_to_die_usec)
 			return (philo_die(philo, 0));
+		pthread_mutex_lock(&philo->table->lock);
 		if (random_bool())
+		{
+			pthread_mutex_unlock(&philo->table->lock);
 			philo_try_fork(philo, &philo->table->fork[philo->id - 1]);
+		}
 		else
 		{
+			pthread_mutex_unlock(&philo->table->lock);
 			if (philo->id == 1)
 				philo_try_fork(philo,
 					&philo->table->fork[philo->table->philo_count - 1]);
